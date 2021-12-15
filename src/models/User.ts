026d0a98357ee136ea6fe73f3e778bb20efaa645
Brandon Ling <brandonvling@gmail.com>
2021-12-15 13:48:40 -0500

    Register and trigger User event callbacks

--- src/models/User.ts
+++ src/models/User.ts
@@ -3,7 +3,10 @@ interface UserProps {
   age: number;
 }
 
+type Callback = () => void;
+
 export class User {
+  events: { [key: string]: Callback[] } = {};
   constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
@@ -15,5 +18,18 @@ export class User {
       ...this.data,
       ...update,
     };
+  }
+
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] ?? [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+    if (!handlers || handlers.length === 0) return;
+
+    handlers.forEach((callback) => callback());
   }
 }
