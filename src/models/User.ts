f23c4660a9027c84ae908a972555197327bf6cfe
Brandon Ling <brandonvling@gmail.com>
2021-12-16 18:20:47 -0500

    Extract Model class
    User now inherits from Model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,7 @@
-import { Eventing } from "./Eventing";
-import { Sync } from "./Sync";
 import { Attributes } from "./Attributes";
-import { AxiosResponse } from "axios";
+import { Model } from "./Model";
+import { Eventing } from "./Eventing";
+import { ApiSync } from "./ApiSync";
 
 interface UserProps {
   id?: number;
@@ -11,51 +11,12 @@ interface UserProps {
 
 const rootUrl = "http://localhost:3000/users";
 
-export class User {
-  events: Eventing = new Eventing();
-  sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-  attributes: Attributes<UserProps>;
-  constructor(attrs: UserProps) {
-    this.attributes = new Attributes<UserProps>(attrs);
-  }
-
-  get on() {
-    return this.events.on;
-  }
-
-  get trigger() {
-    return this.events.trigger;
-  }
-
-  get get() {
-    return this.attributes.get;
-  }
-
-  set(update: Partial<UserProps>) {
-    this.attributes.set(update);
-    this.events.trigger("change");
-  }
-
-  fetch(): void {
-    const id = this.get("id");
-
-    if (typeof id !== "number") {
-      throw new Error("Cannot fetch User without an id");
-    }
-
-    this.sync.fetch(id).then((response: AxiosResponse): void => {
-      this.set(response.data);
-    });
-  }
-
-  save(): void {
-    this.sync
-      .save(this.attributes.getAll())
-      .then((_: AxiosResponse): void => {
-        this.trigger("save");
-      })
-      .catch(() => {
-        this.trigger("error");
-      });
+export class User extends Model<UserProps> {
+  static buildUser(attrs: UserProps) {
+    return new User(
+      new Attributes<UserProps>(attrs),
+      new Eventing(),
+      new ApiSync<UserProps>(rootUrl)
+    );
   }
 }
