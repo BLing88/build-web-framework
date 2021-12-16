42f03f2132355fe0f028b746e0d6a966749ad923
Brandon Ling <brandonvling@gmail.com>
2021-12-16 17:27:30 -0500

    Add fetch User method

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,7 @@
 import { Eventing } from "./Eventing";
 import { Sync } from "./Sync";
 import { Attributes } from "./Attributes";
+import { AxiosResponse } from "axios";
 
 interface UserProps {
   id?: number;
@@ -33,5 +34,17 @@ export class User {
   set(update: Partial<UserProps>) {
     this.attributes.set(update);
     this.events.trigger("change");
+  }
+
+  fetch(): void {
+    const id = this.get("id");
+
+    if (typeof id !== "number") {
+      throw new Error("Cannot fetch User without an id");
+    }
+
+    this.sync.fetch(id).then((response: AxiosResponse): void => {
+      this.set(response.data);
+    });
   }
 }
