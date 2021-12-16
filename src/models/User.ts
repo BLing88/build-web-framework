ff775d7ce7d841b850a2254d587a14f806f7bd64
Brandon Ling <brandonvling@gmail.com>
2021-12-16 14:23:29 -0500

    Extract Eventing class
    Use composition instead of putting everything in User model.

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosResponse } from "axios";
+import { Eventing } from "./Eventing";
 
 interface UserProps {
   id?: number;
@@ -6,10 +7,9 @@ interface UserProps {
   age: number;
 }
 
-type Callback = () => void;
-
 export class User {
-  events: { [key: string]: Callback[] } = {};
+  events: Eventing = new Eventing();
+
   constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
@@ -23,24 +23,21 @@ export class User {
     };
   }
 
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] ?? [];
-    handlers.push(callback);
-    this.events[eventName] = handlers;
-  }
-
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName];
-    if (!handlers || handlers.length === 0) return;
-
-    handlers.forEach((callback) => callback());
-  }
-
   fetch(): void {
     axios
       .get(`http://localhost:3000/users/${this.get("id")}`)
       .then((res: AxiosResponse): void => {
         this.set(res.data);
       });
+  }
+
+  save(): void {
+    const id = this.get("id");
+
+    if (id) {
+      axios.put(`http://localhost:3000/users/${id}`, this.data);
+    } else {
+      axios.post("http://localhost:3000/users/", this.data);
+    }
   }
 }
